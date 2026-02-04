e7ae5839c9901dc71cd051cf070936f709fae1d4
Maulimzhanuly Ramazan <maulimzanulyramazan@gmail.com>
2026-02-04 20:04:13 +0500

    changed while_break example code

--- Practice_2/loops/while_break.py
+++ Practice_2/loops/while_break.py
@@ -1,7 +1,7 @@
 n = int(input())
 while(n > 1):
     if(n % 2 != 0):
-        print(f"half of n is {n / 2} is not even number")
+        print(f"{n / 2} is half of n so n is not even number")
         break
-    print(f"half of n is {n // 2} is even number, let's devide it to 2 again")
+    print(f"{n // 2} is half of n, so n is even number, let's devide it to 2 again")
     n = n // 2

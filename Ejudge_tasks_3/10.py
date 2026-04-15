caf73ba9447af7def5bc48b9fad73d47aa1f3912
maulimzanulyramazan <maulimzanulyramazan@gmail.com>
2026-04-16 03:52:30 +0500

    Delete Ejudge_tasks_3 directory

--- Ejudge_tasks_3/10.py
+++
@@ -1,8 +0,0 @@
-class person():
-    name = ""
-    GPA = 0.0
-student = person()
-a = input().split()
-student.name = a[0]
-student.GPA = float(a[1])
-print(f"Student: {student.name}, GPA: {student.GPA}")
--- Ejudge_tasks_3/5.py
+++
@@ -1,3 +0,0 @@
-def area(num):
-    return num ** 2
-print(area(int(input())))

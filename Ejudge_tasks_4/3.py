2719f1fa77922445c0d4c07f9cfce06f37865da6
maulimzanulyramazan <maulimzanulyramazan@gmail.com>
2026-04-16 03:52:20 +0500

    Delete Ejudge_tasks_4 directory

--- Ejudge_tasks_4/3.py
+++
@@ -1,8 +0,0 @@
-def devisible(n):
-    t = 0
-    while t <= n:
-        yield t
-        t = t + 12
-s = int(input())
-for x in devisible(s):
-    print(x, end =" ")
--- Ejudge_tasks_4/5.py
+++
@@ -1,8 +0,0 @@
-def nums(n):
-    t = n
-    for i in range(t + 1):
-        yield n
-        n = n - 1
-x = int(input())
-for y in nums(x):
-    print(y)
